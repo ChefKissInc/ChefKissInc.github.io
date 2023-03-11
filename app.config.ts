fbeb1cdab975396a7d1b4b6275e092305aea4d9c
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-03-11 07:30:24 +0200

    Initial commit
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+export default defineAppConfig({
+  docus: {
+    title: "Noot",
+    description: "Hackintosh tools that nobody bothered to make",
+    // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
+    socials: {
+      github: "NootInc/"
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    github: {
+      owner: "NootInc",
+      repo: "NootInc.github.io",
+      branch: "master",
+    }
+    // header: {
+    //   logo: true
+    // },
+  }
+})
