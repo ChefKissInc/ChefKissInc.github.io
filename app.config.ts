7ec4d157a04fa11b0040a2b81192b479d90ab5f4
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-03-11 08:04:17 +0200

    Bump dependencies
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
     description: "Hackintosh tools that nobody bothered to make",
     // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
-      github: "NootInc/"
+      github: "NootInc"
     },
     aside: {
       level: 0,
