202ea05699e0eb5ef2b77cc95cd10d4ef6dce209
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-03-29 18:57:55 +0300

    Fix grammatical error in slogan, bump lockfile
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   plugins: ['~/plugins/medium-zoom'],
   docus: {
     title: "Noot",
-    description: "Hackintosh tools that nobody bothered to make",
+    description: "Hackintosh tools that nobody bothered making",
     // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
       github: "NootInc"
