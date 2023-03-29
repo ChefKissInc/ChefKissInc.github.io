ad8c450428e66c65d7135944743976b932ca8e99
Visual <30368284+ChefKissInc@users.noreply.github.com>
2023-03-29 20:48:39 +0300

    Fix z-index of zoomed image
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
+import { createResolver } from '@nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
-  extends: "@nuxt-themes/docus"
+  extends: "@nuxt-themes/docus",
+  css: [
+    resolve('./assets/css/medium-zoom.css')
+  ],
 })
