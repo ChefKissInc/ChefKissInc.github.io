9314ca403d6adf4f591135c45f2e6c579eae7a0d
Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>
2023-06-30 15:08:02 +0300

    Further refine website design
    Signed-off-by: Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -5,11 +5,10 @@ export default defineAppConfig({
     description: "Hackintosh tools that nobody bothered making",
     // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
-      github: "NootInc"
+      github: "NootInc",
     },
     aside: {
-      level: 0,
-      exclude: []
+      level: 1,
     },
     github: {
       owner: "NootInc",
@@ -17,10 +16,20 @@ export default defineAppConfig({
       branch: "master",
       root: "content",
       edit: true,
-      contributors: true
-    }
-    // header: {
-    //   logo: true
-    // },
+      contributors: true,
+    },
+    header: {
+      showLinkIcon: true,
+      fluid: true,
+      //   logo: true,
+    },
+    main: {
+      fluid: true,
+      padded: true,
+    },
+    footer: {
+      fluid: true,
+    },
+    titleTemplate: "%s | Noot",
   }
 })
