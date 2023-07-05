fb5dfae335ac44421df4addf45701c4f332c5633
Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>
2023-07-05 23:19:58 +0300

    Add NRed info and improve homepage
    Signed-off-by: Visual Ehrmanntraut <30368284+ChefKissInc@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,11 @@ export default defineAppConfig({
     description: "Hackintosh tools that nobody bothered making",
     // image: "https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png",
     socials: {
+      telegram: {
+        icon: "mdi:telegram",
+        href: "https://t.me/NootInc",
+        label: "Telegram Supergroup",
+      },
       github: "NootInc",
     },
     aside: {
@@ -29,6 +34,17 @@ export default defineAppConfig({
     },
     footer: {
       fluid: true,
+      credits: {
+        icon: "",
+        text: "Noot Inc and ChefKiss Inc have no affiliation with Dortania, Acidanthera and Apple. macOS is a registered trademark of Apple, Inc.",
+        href: "",
+      },
+      textLinks: [
+        {
+          text: "Be honest. Copyright © 2023 ChefKiss Inc, Noot Inc subdivision. All rights reserved.",
+          href: "https://github.com/ChefKissInc",
+        },
+      ]
     },
     titleTemplate: "%s | Noot",
   }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,20 @@ import { createResolver } from '@nuxt/kit'
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
+  alias: {},
   extends: "@nuxt-themes/docus",
   css: [
     resolve('./assets/css/medium-zoom.css')
   ],
-})
+  colorMode: {
+    preference: 'dark',
+  },
+  runtimeConfig: {
+    content: {
+      // @ts-ignore
+      documentDriven: {
+        host: 'https://nootinc.github.io',
+      },
+    },
+  },
+});
