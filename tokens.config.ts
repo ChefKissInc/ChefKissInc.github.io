5ebe69ed4c46745b627888950f18620372345b2e
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-05-11 02:09:13 +0300

    Slightly alter dark mode colour scheme

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,3 +1,28 @@
 import { defineTheme } from 'pinceau'
 
-export default defineTheme({});
+export default defineTheme({
+  color: {
+    black: '#000000',
+  },
+  docus: {
+    header: {
+      height: '48px',
+    },
+  },
+  elements: {
+    backdrop: {
+      background: {
+        initial: '#fffc',
+        dark: 'rgba(27, 27, 27, 0.98)',
+      },
+    },
+    border: {
+      primary: {
+        static: {
+          initial: '{color.gray.100}',
+          dark: 'rgba(149, 149, 154, 0.7)',
+        },
+      },
+    },
+  },
+});
