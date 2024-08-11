c98ea55baaabdaa14583c1994ceff38deeaaffde
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-08-11 17:10:55 +0300

    Remove ugly cover background

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,5 +1,6 @@
 import { getCollection } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
+import type { OGImageOptions } from "node_modules/astro-og-canvas/dist/types";
 
 const entries = await getCollection("docs");
 const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
@@ -7,37 +8,49 @@ const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
 export const { getStaticPaths, GET } = OGImageRoute({
   pages,
   param: "slug",
-  getImageOptions: (_path, page) => {
+  getImageOptions: (path, page) => {
+    let opts: OGImageOptions = {
+      title: page.data.title,
+      description: page.data.description,
+      dir: path.split("/")[0] === "ar" ? "rtl" : "ltr",
+      padding: 120,
+      font: {
+        title: {
+          families: ["Inter"],
+        },
+        description: {
+          families: ["Inter"],
+        },
+      },
+      fonts: [
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/latin-400-normal.woff2",
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/latin-ext-400-normal.woff2",
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/greek-400-normal.woff2",
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/greek-ext-400-normal.woff2",
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/cyrillic-400-normal.woff2",
+        "https://cdn.jsdelivr.net/fontsource/fonts/inter@latest/cyrillic-ext-400-normal.woff2",
+      ],
+      quality: 100,
+    };
     const cover = page.data.cover
       ? "dark" in page.data.cover
         ? page.data.cover.dark
         : page.data.cover.image
       : undefined;
 
-    return {
-      title: page.data.title,
-      description: page.data.description,
-      padding: 120,
-      bgImage: {
-        path: cover
-          ? "fsPath" in cover
-            ? cover.fsPath
-            : cover.src
-          : "./src/assets/CoverBackground.png",
+    if (cover) {
+      opts.bgImage = {
+        path: "fsPath" in cover ? cover.fsPath : cover.src,
         fit: "fill",
-      },
-      font: {
-        title: {
-          families: ["Inter"],
-        },
-        description: {
-          families: ["JetBrains Mono"],
-        },
-      },
-      fonts: [
-        "https://api.fontsource.org/v1/fonts/inter/latin-400-normal.ttf",
-        "https://api.fontsource.org/v1/fonts/jetbrains-mono/latin-400-normal.ttf",
-      ],
-    };
+      };
+    } else {
+      opts.bgGradient = [[15, 15, 15]];
+      opts.border = {
+        color: [16, 185, 129],
+        side: "block-end",
+        width: 16,
+      };
+    }
+    return opts;
   },
 });
