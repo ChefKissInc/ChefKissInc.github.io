e19d441698d9f622ceb10e2ecb49c00de4ad4b44
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-09-28 15:55:26 +0300

    fix: Work around build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,6 +38,7 @@ export default defineConfig({
         starlightLinksValidator({
           errorOnFallbackPages: false,
           errorOnInconsistentLocale: true,
+          exclude: ["(/*)?/newsroom/"], // TODO: Remove this once Newsroom v2 lands!!
         }),
       ],
       title: "ChefKiss",
