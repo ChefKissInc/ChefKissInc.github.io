ffb8242fe421cbfd145cc30a59680a560e4a6ebb
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-10-20 12:30:26 +0300

    docs: update comment in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,7 +38,7 @@ export default defineConfig({
         starlightLinksValidator({
           errorOnFallbackPages: false,
           errorOnInconsistentLocale: true,
-          exclude: ["(/*)?/newsroom/"], // TODO: Remove this once Newsroom v2 lands!!
+          exclude: ["(/*)?/newsroom/"], // TODO: Remove this
         }),
       ],
       title: "ChefKiss",
